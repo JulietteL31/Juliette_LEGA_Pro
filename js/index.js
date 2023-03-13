dd844871b65713f05d0cd5bd8ef3c162c015f459
Juliette Lega <legajuliette@gmail.com>
2023-03-13 23:01:12 +0100

    modif photo page d'accueil et suppr nav mobile dans fichier index.js

--- js/index.js
+++ js/index.js
@@ -104,16 +104,3 @@ function AffichePort() {
 }
 
 flecheVersPort.addEventListener("click", AffichePort)
-
-//Navigation mobile
-let bouton = document.getElementById('bouton');
-let affiche = document.getElementById('display');
-let nav = document.querySelector('.nav');
-
-bouton.addEventListener("click", () => {
-    if(getComputedStyle(nav).display != "block"){      
-      nav.style.display = "block";
-    } else {
-      nav.style.display = "none";
-    }
-  })
