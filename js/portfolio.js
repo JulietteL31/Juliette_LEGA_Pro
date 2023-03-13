9fb1948830f4e28cf218e6c831b676c12f5fad2d
Juliette Lega <legajuliette@gmail.com>
2023-03-13 17:05:22 +0100

    ajout fichier js pour nav mobile sur page portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+//Navigation mobile
+let bouton = document.getElementById('bouton');
+let affiche = document.getElementById('display');
+let nav = document.querySelector('.nav');
+
+bouton.addEventListener("click", () => {
+    if(getComputedStyle(nav).display != "block"){      
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+  })
