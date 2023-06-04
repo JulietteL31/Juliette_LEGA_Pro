2272ece03f9eb6638f7fbafbb48f1d373d093fea
Juliette Lega <legajuliette@gmail.com>
2023-06-04 23:32:21 +0200

    nouvel affichage accueil

--- js/index.js
+++ js/index.js
@@ -73,7 +73,7 @@ let titreComp = document.getElementById('titreComp');
 let technique = document.getElementById('divTechnique');
 let soft = document.getElementById('divSoft');
 let gestion = document.getElementById('divGestion');
-let socio = document.getElementById('divSocio');
+// let socio = document.getElementById('divSocio');
 
 function AfficheComp() {
     sectionComp.style.display = "flex"
@@ -81,7 +81,7 @@ function AfficheComp() {
     technique.style.animationPlayState = "running"
     soft.style.animationPlayState = "running"
     gestion.style.animationPlayState = "running"
-    socio.style.animationPlayState = "running"
+    // socio.style.animationPlayState = "running"
     ligneComp.style.animationPlayState = "running"
     flecheVersComp.style.display = "none"
     // recommandation2.style.display = "flex"
@@ -92,18 +92,43 @@ flecheVersComp.addEventListener("click", AfficheComp)
 //AFFICHAGE SECTION PORTFOLIO
 let flecheVersPort = document.getElementById('flecheVersPort');
 let sectionPortfolio = document.getElementById('section-portfolio-index');
-let titrePort = document.getElementById('titrePort')
-let projetFormation = document.getElementById('projet-formation')
-let projetPerso = document.getElementById('projet-perso')
-let recommandation = document.getElementById('recommandation');
+let titrePort = document.getElementById('titrePort');
+let titreReco= document.getElementById('titreReco');
+let titreAbout = document.getElementById('titreAbout');
+let projetFormation = document.getElementById('projet-formation');
+let projetPerso = document.getElementById('projet-perso');
+let ligneReco = document.getElementById('ligne-titre-reco');
+let ligneAbout = document.getElementById('ligne-titre-about');
+let aboutPremiere = document.getElementById('aboutPremiere');
+let about2 = document.getElementById('about2');
+let about3 = document.getElementById('about3');
+let about4 = document.getElementById('about4');
+let about5 = document.getElementById('about5');
+let about6 = document.getElementById('about6');
+let about7 = document.getElementById('about7');
+let about8 = document.getElementById('about8');
+let about9 = document.getElementById('about9');
 
 function AffichePort() {
     sectionPortfolio.style.display = "flex"
     lignePortfolio.style.animationPlayState = "running"
+    ligneAbout.style.animationPlayState = "running"
+    ligneReco.style.animationPlayState = "running"
     flecheVersPort.style.display = "none"
     titrePort.style.animationPlayState = "running"
-    projetFormation.style.animationPlayState = "running"
-    projetPerso.style.animationPlayState = "running"
+    titreReco.style.animationPlayState = "running"
+    titreAbout.style.animationPlayState = "running"
+    // projetFormation.style.animationPlayState = "running"
+    // projetPerso.style.animationPlayState = "running"
+    aboutPremiere.style.animationPlayState = "running"
+    about2.style.animationPlayState = "running"
+    about3.style.animationPlayState = "running"
+    about4.style.animationPlayState = "running"
+    about5.style.animationPlayState = "running"
+    about6.style.animationPlayState = "running"
+    about7.style.animationPlayState = "running"
+    about8.style.animationPlayState = "running"
+    about9.style.animationPlayState = "running"
     // recommandation.style.display = "flex"
 
 }
@@ -114,47 +139,47 @@ flecheVersPort.addEventListener("click", AffichePort)
 //AFFICHAGE RECOMMANDATIONS
 
 //Recommandation 1
-let divReco = document.getElementById('div-reco');
-let bulle = document.getElementById("bulle");
-let reco = document.getElementById("reco");
-let pointsReco = document.getElementById("points-reco")
+// let divReco = document.getElementById('div-reco');
+// let bulle = document.getElementById("bulle");
+// let reco = document.getElementById("reco");
+// let pointsReco = document.getElementById("points-reco")
 
-function AfficheReco() {
-    bulle.style.display = "none";
-    reco.className = "p-reco";
-    reco.style.animationPlayState = "running";
-}
+// function AfficheReco() {
+//     bulle.style.display = "none";
+//     reco.className = "p-reco";
+//     reco.style.animationPlayState = "running";
+// }
 
-bulle.addEventListener('click', AfficheReco);
-pointsReco.addEventListener('click', AfficheReco);
+// bulle.addEventListener('click', AfficheReco);
+// pointsReco.addEventListener('click', AfficheReco);
 
-function FermeReco() {
-    bulle.style.display = "block";
-    reco.className = "recoFerme";
-    reco.style.animationPlayState = "running";
-}
+// function FermeReco() {
+//     bulle.style.display = "block";
+//     reco.className = "recoFerme";
+//     reco.style.animationPlayState = "running";
+// }
 
-reco.addEventListener('click', FermeReco);
+// reco.addEventListener('click', FermeReco);
 
 //Recommandation 2
-let divReco2 = document.getElementById('div-reco2');
-let bulle2 = document.getElementById("bulle2");
-let reco2 = document.getElementById("reco2");
-let pointsReco2 = document.getElementById("points-reco2")
+// let divReco2 = document.getElementById('div-reco2');
+// let bulle2 = document.getElementById("bulle2");
+// let reco2 = document.getElementById("reco2");
+// let pointsReco2 = document.getElementById("points-reco2")
 
-function AfficheReco2() {
-    bulle2.style.display = "none";
-    reco2.className = "p-reco";
-    reco2.style.animationPlayState = "running";
-}
+// function AfficheReco2() {
+//     bulle2.style.display = "none";
+//     reco2.className = "p-reco";
+//     reco2.style.animationPlayState = "running";
+// }
 
-bulle2.addEventListener('click', AfficheReco2);
-pointsReco2.addEventListener('click', AfficheReco2);
+// bulle2.addEventListener('click', AfficheReco2);
+// pointsReco2.addEventListener('click', AfficheReco2);
 
-function FermeReco2() {
-    bulle2.style.display = "block";
-    reco2.className = "recoFerme";
-    reco2.style.animationPlayState = "running";
-}
+// function FermeReco2() {
+//     bulle2.style.display = "block";
+//     reco2.className = "recoFerme";
+//     reco2.style.animationPlayState = "running";
+// }
 
-reco2.addEventListener('click', FermeReco2);
+// reco2.addEventListener('click', FermeReco2);
